771d0a0c970e96e4a5a774530bde514beb9259d5
Umaralikhon <umaralikhon@gmail.com>
2021-06-23 14:18:39 +0700

    Add files via upload

---
+++ src/resources/basket.sql
@@ -0,0 +1,7 @@
+CREATE TABLE basket(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    sum int;
+    cost int;
+    PRIMARY KEY(id),
+);
---
+++ src/resources/pills.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pills(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    amount int;
+    cost int;
+    PRIMARY KEY(id),
+);
